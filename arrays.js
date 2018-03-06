0bf8a2ca6af95dfac1ffce63306fbfa16984b1fb
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-06 16:48:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  if(!element) {
-    var element = 'foo';
-  }
   [element, ...array];
 }
