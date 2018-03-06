c080bba6ea146414b0fd3b00e3462201776bff35
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-06 16:57:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,13 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var newArray = array.slice(1);
+  return newArray
+}
